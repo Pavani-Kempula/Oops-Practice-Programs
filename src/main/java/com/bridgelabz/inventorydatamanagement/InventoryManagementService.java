5294d38b89a6a8285e232a83d2b5f2bf84b9ae39
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-25 17:37:48 +0530

    ADDED : Inventory data management project thats add items,display items and Calculating inventory value for the user

---
+++ src/main/java/com/bridgelabz/inventorydatamanagement/InventoryManagementService.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.inventorydatamanagement;
+
+/*
+ * @author Pavani
+ *
+ * This interface will have abstract methods which we will be using
+ * for inventory data manipulation.
+ */
+
+public interface InventoryManagementService {
+    public void addInventoryItems(Items items);
+    public void calculateValueOfInventory();
+    public void printItemNames();
+    public void printSelectedItemDetails(String userItemName);
+    public void printAllItemsDetails();
+}
