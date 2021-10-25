8c2a1d749f896a02919b5ae9ae3f230314bd3355
PavaniKempula <pavanikempula1998@gmail.com>
2021-10-25 17:19:12 +0530

    ADDED : Shuffling cards from deck of cards and distributing 9 cards to each four players

---
+++ src/main/java/com/bridgelabz/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.deckofcards;
+/*
+ * @ author Pavani
+ *
+ * This class is a main class from here
+ * we will call the methods we are using in the program.
+ */
+
+public class DeckOfCardsMain {
+   public static void main(String[] args) {
+      DeckOfCards deckOfCardsObject = new DeckOfCards();
+      deckOfCardsObject.deckInitialize();
+      deckOfCardsObject.shuffle();
+      deckOfCardsObject.PrintDistribute();
+   }
+}
